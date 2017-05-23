a20f3ab95685c5a4f6e312f07f605fa6918f1758
pestree <pestree@PC-DG-021-17.ntiut2gre.iut2.upmf-grenoble.fr>
2017-05-23 10:33:35 +0200

    juste un test

--- Ile-Interdite/src/ile/interdite/IleInterdite.java
+++ Ile-Interdite/src/ile/interdite/IleInterdite.java
@@ -18,6 +18,7 @@ public class IleInterdite {
         System.out.println("Hello");
         System.out.println("test d'ajout de ligne par envoi via NetB");
         System.out.println("Test devant tout le monde");
+        System.out.println("moi aussi");
     }
     
 }
