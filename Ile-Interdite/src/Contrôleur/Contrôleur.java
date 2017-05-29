18bf44ba955e8c15cb5c26a915e5cdc0c4b4b9a7
robinta <robinta@pc-dg-039-12>
2017-05-29 14:58:32 +0200

    Correction

--- Ile-Interdite/src/Contrôleur/Contrôleur.java
+++ Ile-Interdite/src/Contrôleur/Contrôleur.java
@@ -26,6 +26,42 @@ public class Contrôleur {
         this.grille=grille;
         this.vueAventurier=vueAventurier;        
     }
+
+    public HashMap<String, Aventurier> getJoueurs() {
+        return joueurs;
+    }
+
+    public Grille getGrille() {
+        return grille;
+    }
+
+    public VueAventurier getVueAventurier() {
+        return vueAventurier;
+    }
+
+    public void setJoueurs(HashMap<String, Aventurier> joueurs) {
+        this.joueurs = joueurs;
+    }
+
+    public void setGrille(Grille grille) {
+        this.grille = grille;
+    }
+
+    public void setVueAventurier(VueAventurier vueAventurier) {
+        this.vueAventurier = vueAventurier;
+    }
+    
+    public void tour(Aventurier a){
+        
+        int nbAction = 3;
+        
+        while (nbAction > 0) {
+            
+        }
+    }
+    
+    public void effectuerAction(){
+    }
     
    
 }
