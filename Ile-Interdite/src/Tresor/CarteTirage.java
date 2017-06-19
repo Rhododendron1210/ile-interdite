bd293cc898875d1f75749dac6472687b3885770e
pestree <pestree@pc-dg-025-09>
2017-06-19 15:27:50 +0200

    grille aleatoire

---
+++ Ile-Interdite/src/Tresor/CarteTirage.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tresor;
+
+/**
+ *
+ * @author pestree
+ */
+public class CarteTirage {
+    private String nom;
+    private String description;
+
+    public CarteTirage(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+    
+    
+}
