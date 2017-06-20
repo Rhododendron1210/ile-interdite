56fa89db429d6f158da6a961ddfa755d65a116c5
louesdol <louesdol@pc-dg-025-08>
2017-06-20 10:09:34 +0200

    version clean

--- ile-interdite/Ile-Interdite/src/view/VueTuile.java
+++ ile-interdite/Ile-Interdite/src/view/VueTuile.java
@@ -1,28 +1,40 @@
 package view;
  
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Insets;
-import javax.swing.BorderFactory;
+import java.awt.GridLayout;
+import java.util.HashMap;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
 import model.Tuile;
+import model.aventuriers.Aventurier;
 import static util.Utils.EtatTuile.ASSECHEE;
 import static util.Utils.EtatTuile.INONDEE;
 
-public class VueTuile extends JPanel {
+public class VueTuile extends JButton{
     
     VueTuile(Tuile tuile){
         JLabel label = new JLabel(tuile.getNom());
-        this.add(label);
+        this.setLayout(new BorderLayout());
+        this.add(label,BorderLayout.NORTH);
         if (tuile.getEtatTuile()==ASSECHEE){
             this.setBackground(Color.orange);
         } else if (tuile.getEtatTuile()==INONDEE){
             this.setBackground(Color.BLUE);
         } else {
             this.setBackground(Color.gray);
+            this.setEnabled(false);
         }
+        JPanel panel = new JPanel(new GridLayout(1,4));
+        HashMap<String,Aventurier> aventuriers = tuile.getAventurierPresent();
+        for (Aventurier ave: aventuriers.values()){
+            if (ave!=null){
+                
+            }
+        }
+        this.add(panel, BorderLayout.CENTER);
     }  
+
+    
 }
