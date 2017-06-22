47f4d32866e36ec83daca6075bf30a18d0c3c24e
epalley <epalley@pc-dg-025-10>
2017-06-22 10:50:39 +0200

    test

--- ile-interdite/Ile-Interdite/src/view/VueDefausse.java
+++ ile-interdite/Ile-Interdite/src/view/VueDefausse.java
@@ -5,14 +5,46 @@
  */
 package view;
 
+import Tresor.CarteTirage;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 /**
  *
  * @author epalley
  */
 public class VueDefausse extends JFrame{
-    VueDefausse(int nbDefausse){
+    
+    private int nbCarte;
+    
+    VueDefausse(int nbCarte, ArrayList<CarteTirage> cartes){
+        
         JFrame window = new JFrame("Défausse des cartes");
+        window.setSize(600,300);
+        
+        JPanel panel = new JPanel(new GridLayout(1,nbCarte));
+        
+        JScrollPane paneDeroulant = new JScrollPane(panel);
+        paneDeroulant.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        
+        while(nbCarte>10){
+            
+            for(int i = nbCarte; i>10; i++){
+                
+                JTextPane pane = new JTextPane();
+                panel.add(pane);
+                
+                //Iterator ite = cartes.iterator();
+                //pane.add(carte);
+            }
+        }
+        
+        
     }
 }
